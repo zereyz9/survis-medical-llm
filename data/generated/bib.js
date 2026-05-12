27294ba46a060cc1fd1314ac3124c36606f8128a
zereyz9 <2356481957@qq.com>
2026-05-12 14:42:58 +0800

    Add files via upload

---
+++ data/generated/bib.js
@@ -0,0 +1,176 @@
+﻿const generatedBibEntries = {
+    "bian2026medscope": {
+        "author": "Bian, Rui and Cheng, Weihao",
+        "doi": "10.64898/2026.03.31.26349827v1",
+        "journal": "medRxiv",
+        "keywords": "type:clinical-diagnosis",
+        "title": "MedScope: A Lightweight Benchmark of Open-Source Large Language Models for Medical Question Answering",
+        "type": "article",
+        "year": "2026"
+    },
+    "colelough2026hallucinations": {
+        "author": "Colelough, Brandon C. and Bartels, Davis and Demner-Fushman, Dina",
+        "doi": "10.48550/arXiv.2603.09986",
+        "journal": "arXiv preprint",
+        "keywords": "type:trustworthiness",
+        "title": "Quantifying Hallucinations in Large Language Models on Medical Textbooks",
+        "type": "article",
+        "year": "2026"
+    },
+    "gong2026applications": {
+        "author": "Gong, Eun Jeong and Bang, Chang Seok and Shin, Yong Seok",
+        "doi": "10.3390/bioengineering13030365",
+        "journal": "Bioengineering",
+        "keywords": "type:text-classification",
+        "number": "3",
+        "pages": "365",
+        "title": "Applications of Large Language Models in Medical Research: From Systematic Reviews to Clinical Studies",
+        "type": "article",
+        "volume": "13",
+        "year": "2026"
+    },
+    "gorenshtein2026neurology": {
+        "author": "Gorenshtein, Alon and Shihada, Kamel and Omar, Mahmud and Klang, Eyal",
+        "doi": "10.21203/rs.3.rs-8902070/v1",
+        "journal": "Research Square",
+        "keywords": "type:clinical-diagnosis",
+        "title": "Large Language Models in Clinical Neurology: A Systematic Review",
+        "type": "article",
+        "year": "2026"
+    },
+    "klang2026admin": {
+        "author": "Klang, Eyal and Gorenshtein, Alon and Shihada, Kamel and Omar, Mahmud and Glicksberg, Benjamin S.",
+        "doi": "10.1371/journal.pdig.0001326",
+        "journal": "PLOS Digital Health",
+        "keywords": "type:text-classification",
+        "number": "5",
+        "pages": "e0001326",
+        "title": "Large language models are poor clinical administrators: An evaluation of structured queries in real-world electronic health records",
+        "type": "article",
+        "volume": "5",
+        "year": "2026"
+    },
+    "lu2026mhb": {
+        "author": "Lu, Jianrong and Liu, Junwei and Zheng, Xingyun and Yang, Minghui and Wang, Jian and Wang, Ping and Zhang, Yechao",
+        "doi": "10.1609/aaai.v40i45.41243",
+        "journal": "Proceedings of the AAAI Conference on Artificial Intelligence",
+        "keywords": "type:trustworthiness",
+        "number": "45",
+        "pages": "38971--38978",
+        "title": "MHB: Medical Hallucination Benchmark for Large Language Models in Complex Clinical tasks",
+        "type": "article",
+        "volume": "40",
+        "year": "2026"
+    },
+    "nori2023gpt4": {
+        "author": "Nori, Harsha and King, Nicholas and McKinney, Scott Mayer and Carignan, Dean and Horvitz, Eric",
+        "doi": "10.48550/arXiv.2303.13375",
+        "journal": "arXiv preprint",
+        "keywords": "type:trustworthiness",
+        "title": "Capabilities of GPT-4 on Medical Challenge Problems",
+        "type": "article",
+        "year": "2023"
+    },
+    "rao2026scars": {
+        "author": "Rao, Mingjun and Tang, Xiujun and Wang, Haoyu",
+        "doi": "10.2196/78838",
+        "journal": "JMIR Medical Informatics",
+        "keywords": "type:trustworthiness",
+        "pages": "e78838",
+        "title": "Evaluating GPT-4 Responses on Scars or Keloids for Patient Education: Large Language Model Evaluation Study",
+        "type": "article",
+        "volume": "14",
+        "year": "2026"
+    },
+    "sakai2026llm": {
+        "author": "Sakai, Hajar and Lam, Sarah S.",
+        "doi": "10.2196/79202",
+        "journal": "JMIR AI",
+        "keywords": "type:text-classification",
+        "pages": "e79202",
+        "title": "Large Language Models for Health Care Text Classification: Systematic Review",
+        "type": "article",
+        "volume": "5",
+        "year": "2026"
+    },
+    "sim2025impure": {
+        "author": "Sim, Shamus Zi Yang and Chen, Tyrone",
+        "doi": "10.7554/eLife.106187",
+        "journal": "eLife",
+        "keywords": "type:clinical-diagnosis",
+        "pages": "e106187",
+        "title": "Critique of impure reason: Unveiling the reasoning behaviour of medical large language models",
+        "type": "article",
+        "volume": "14",
+        "year": "2025"
+    },
+    "singhal2023expert": {
+        "author": "Singhal, Karan and Tu, Tao and Gottweis, Juraj and Sayres, Rory and Wulczyn, Ellery and Amin, Mitra and Clark, Kevin and Pfohl, Stephen R. and Cole-Lewis, Heather and others",
+        "doi": "10.48550/arXiv.2305.09617",
+        "journal": "arXiv preprint",
+        "keywords": "type:clinical-diagnosis",
+        "title": "Towards Expert-Level Medical Question Answering with Large Language Models",
+        "type": "article",
+        "year": "2023"
+    },
+    "singhal2023nature": {
+        "author": "Singhal, Karan and Azizi, Shekoofeh and Tu, Tao and Mahdavi, S. Sara and Wei, Jason and Chung, Hyung Won and Scales, Nathan and Tanwani, Ajay and Cole-Lewis, Heather and Pfohl, Stephen and Payne, Perry and Seneviratne, Martin and Gamble, Paul and Kelly, Chris and Babiker, Abdul and Sch{\\\"a}rli, Nathanael and Chowdhery, Aakanksha and Mansfield, Philip and Demner-Fushman, Dina and Ag{\\\"u}era y Arcas, Blaise and Webster, Dale and Corrado, Greg S. and Matias, Yossi and Chou, Katherine and Gottweis, Juraj and Tomasev, Nenad and Liu, Yun and Rajkomar, Alvin and Barral, Joelle and Semturs, Christopher and Karthikesalingam, Alan and Natarajan, Vivek",
+        "doi": "10.1038/s41586-023-06291-2",
+        "journal": "Nature",
+        "keywords": "type:clinical-diagnosis",
+        "number": "7972",
+        "pages": "172--180",
+        "title": "Large language models encode clinical knowledge",
+        "type": "article",
+        "volume": "620",
+        "year": "2023"
+    },
+    "thirunavukarasu2023medicine": {
+        "author": "Thirunavukarasu, Arun James and Ting, Darren Shu Jeng and Elangovan, Kabilan and Gutierrez, Laura and Tan, Ting Fang and Ting, Daniel Shu Wei",
+        "doi": "10.1038/s41591-023-02448-8",
+        "journal": "Nature Medicine",
+        "keywords": "type:clinical-diagnosis",
+        "number": "8",
+        "pages": "1930--1940",
+        "title": "Large language models in medicine",
+        "type": "article",
+        "volume": "29",
+        "year": "2023"
+    },
+    "wang2026survey": {
+        "author": "Wang, Qiting and Zou, Huiru and Zhang, Haobin and Huang, Yongshun and Tian, Junzhang and Cheng, Weibin",
+        "doi": "10.1002/hcs2.70050",
+        "journal": "Health Care Science",
+        "keywords": "type:clinical-diagnosis",
+        "number": "1",
+        "pages": "4--18",
+        "title": "A Survey on Medical Competence Evaluation Benchmarks for Large Language Models",
+        "type": "article",
+        "volume": "5",
+        "year": "2026"
+    },
+    "yang2022gatortron": {
+        "author": "Yang, Xi and Chen, Aokun and PourNejatian, Nima and Shin, Hoo Chang and Smith, Kaleb E. and Parisien, Christopher and Compas, Colin and others",
+        "doi": "10.1038/s41746-022-00742-2",
+        "journal": "npj Digital Medicine",
+        "keywords": "type:text-classification",
+        "number": "1",
+        "pages": "194",
+        "title": "GatorTron: A large language model for electronic health records",
+        "type": "article",
+        "volume": "5",
+        "year": "2022"
+    },
+    "yu2025challenges": {
+        "author": "Yu, Erlan and Chu, Xuehong and Zhang, Wanwan and Meng, Xiangbin and Yang, Yiran and Ji, Xunming and Wu, Chuanjie",
+        "doi": "10.7150/ijms.111780",
+        "journal": "International Journal of Medical Sciences",
+        "keywords": "type:trustworthiness",
+        "number": "11",
+        "pages": "2792--2801",
+        "title": "Large Language Models in Medicine: Applications, Challenges, and Future Directions",
+        "type": "article",
+        "volume": "22",
+        "year": "2025"
+    }
+};
